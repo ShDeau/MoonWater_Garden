d0d6a6ffd57e0f654c4e3583d8d8c389d8ee079b
ShaDeau <damien@sporring.com>
2026-01-27 08:01:45 +0100

    Add files via upload
    Starting working on a map editor

--- main.v
+++ main.v
@@ -31,11 +31,24 @@ mut:
     tile_size int
 }
 
+enum MapEditorPlacingModes {
+    tiles
+    small_plants
+    medium_plants
+    big_plants
+    background_im
+    foreground_im
+}
+
+struct MapEditor {
+    placing MapEditorPlacingModes
+}
+
 
 
 struct App {
 mut:
-    gg    &gg.Context = unsafe { nil }
+    ctx    &gg.Context = unsafe { nil }
     map Map
     window_width int
     window_height int
@@ -44,7 +57,7 @@ mut:
 
 fn main() {
     mut app := &App{}
-    app.gg = gg.new_context(
+    app.ctx = gg.new_context(
         create_window: true
         fullscreen: true
         window_title: '- Application -'
@@ -56,7 +69,7 @@ fn main() {
     )
 
     //lancement du programme/de la fenêtre
-    app.gg.run()
+    app.ctx.run()
 }
 
 fn on_frame(mut app App) {
@@ -65,47 +78,76 @@ fn on_frame(mut app App) {
     app.map.tile_size = app.window_height / dim
 
     $if editor ? {
-
+        app.map_editor()
     }
-
-    //Draw
-    app.gg.begin()
-    $if editor ? {
-        app.draw_grid()
-    }
-    app.gg.end()
 }
 
 fn on_event(e &gg.Event, mut app App){
+    $if editor ? {
+        app.map_editor_on_event(e)
+    } $else {
+        match e.typ {
+            .key_down {
+                match e.key_code {
+                    .escape {app.ctx.quit()}
+                    else {}
+                }
+            }
+            .mouse_down {
+                match e.mouse_button{
+                    .left{
+                    }
+                    else{}
+            }}
+            else {}
+        }
+    }
+}
+
+fn (app App) draw_grid() {
+    for i in 1..dim {
+        app.ctx.draw_line(i*app.map.tile_size, 0, i*app.map.tile_size, app.window_height, gg.black)
+        app.ctx.draw_line(0, i*app.map.tile_size, app.map.tile_size*dim, i*app.map.tile_size, gg.black)
+    }
+}
+
+fn (app App) draw_square_selected() {
+    pos_x := (app.ctx.mouse_pos_x / app.map.tile_size) * app.map.tile_size
+    pos_y := (app.ctx.mouse_pos_y / app.map.tile_size) * app.map.tile_size
+    app.ctx.draw_square_filled(pos_x, pos_y, app.map.tile_size, gg.gray)
+}
+
+fn (app App) map_editor () {
+    app.map_editor_on_frame()
+    app.draw_map_editor()
+}
+
+fn (app App) map_editor_on_frame () {
+}
+
+fn (app App) draw_map_editor () {
+    app.ctx.begin()
+    app.draw_grid()
+    app.draw_square_selected()
+    app.ctx.end()
+}
+
+fn (app App) map_editor_on_event (e &gg.Event) {
     match e.typ {
         .key_down {
             match e.key_code {
-                .escape {app.gg.quit()}
+                .escape {app.ctx.quit()}
                 else {}
             }
         }
         .mouse_down {
             match e.mouse_button{
                 .left{
-                    app.draw_square_selected()
                 }
                 else{}
         }}
         else {}
     }
-}
-
-fn (app App) draw_grid() {
-    for i in 1..dim {
-        app.gg.draw_line(i*app.map.tile_size, 0, i*app.map.tile_size, app.window_height, gg.black)
-        app.gg.draw_line(0, i*app.map.tile_size, app.map.tile_size*dim, i*app.map.tile_size, gg.black)
-    }
-}
-
-fn (app App) draw_square_selected() {
-    pos_x := (app.gg.mouse_pos_x / app.map.tile_size) * app.map.tile_size
-    pos_y := (app.gg.mouse_pos_y / app.map.tile_size) * app.map.tile_size
-    app.gg.draw_square_filled(pos_x, pos_y, app.map.tile_size, gg.gray)
 }
 
 // fn (ctx &Context) draw_line(x f32, y f32, x2 f32, y2 f32, c Color)
