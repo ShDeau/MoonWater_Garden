81f10d9639c7b5be2ba2c029aaa812382453974f
ShaDeau <damien@sporring.com>
2026-01-28 12:40:02 +0100

    Map_editor progress

--- main.v
+++ main.v
@@ -4,7 +4,7 @@ const bg_color     = gg.blue
 const dim = 20
 
 enum Tiles {
-    default
+    default = 0
 }
 
 struct Small_plants {}
@@ -29,6 +29,10 @@ mut:
     background []Images
     foreground []Images
     tile_size int
+    tiles_im []gg.Image
+    small_plants_im []gg.Image
+    med_plants_im []gg.Image
+    big_plants_im []gg.Image
 }
 
 enum MapEditorPlacingModes {
@@ -42,6 +46,7 @@ enum MapEditorPlacingModes {
 
 struct MapEditor {
     placing MapEditorPlacingModes
+    number_placing int
 }
 
 
@@ -50,6 +55,7 @@ struct App {
 mut:
     ctx    &gg.Context = unsafe { nil }
     map Map
+    map_ed MapEditor
     window_width int
     window_height int
 }
@@ -67,6 +73,10 @@ fn main() {
         event_fn: on_event
         sample_count: 2
     )
+
+    app.map.tiles_im << app.ctx.create_image('default_tile.png') or { panic(err) }
+
+    app.map.tiles = [][]Tiles{len:20, init:[]Tiles{len:20, init:.default}}
 
     //lancement du programme/de la fenêtre
     app.ctx.run()
@@ -112,9 +122,9 @@ fn (app App) draw_grid() {
 }
 
 fn (app App) draw_square_selected() {
-    pos_x := (app.ctx.mouse_pos_x / app.map.tile_size) * app.map.tile_size
-    pos_y := (app.ctx.mouse_pos_y / app.map.tile_size) * app.map.tile_size
-    app.ctx.draw_square_filled(pos_x, pos_y, app.map.tile_size, gg.gray)
+    pos_x := app.tile_x_to_px(app.m_tile_x())
+    pos_y := app.tile_x_to_px(app.m_tile_y())
+    app.ctx.draw_square_filled(pos_x, pos_y, app.map.tile_size, gg.Color{200, 200, 200, 70})
 }
 
 fn (app App) map_editor () {
@@ -128,21 +138,32 @@ fn (app App) map_editor_on_frame () {
 fn (app App) draw_map_editor () {
     app.ctx.begin()
     app.draw_grid()
+    app.draw_map()
     app.draw_square_selected()
     app.ctx.end()
 }
 
-fn (app App) map_editor_on_event (e &gg.Event) {
+fn (mut app App) map_editor_on_event (e &gg.Event) {
     match e.typ {
         .key_down {
             match e.key_code {
                 .escape {app.ctx.quit()}
+                .p {
+                    for y in 0..app.map.tiles.len {
+                        for x in 0..app.map.tiles[y].len {
+                            print("${app.map.tiles[y][x]} ")
+                        }
+                        print("\n")
+                    }
+                    print("\n")
+                }
                 else {}
             }
         }
         .mouse_down {
             match e.mouse_button{
                 .left{
+                    app.place()
                 }
                 else{}
         }}
@@ -150,5 +171,54 @@ fn (app App) map_editor_on_event (e &gg.Event) {
     }
 }
 
+fn (mut app App) place () {
+    match app.map_ed.placing {
+        .tiles{
+            app.map.tiles[app.m_tile_y()][app.m_tile_x()] = Tiles.from(app.map_ed.number_placing) or {Tiles.default}
+        }
+        else{}
+    }
+}
+
+fn (app App) m_tile_x () int {
+    return (app.ctx.mouse_pos_x / app.map.tile_size)
+}
+
+fn (app App) m_tile_y () int {
+    return (app.ctx.mouse_pos_y / app.map.tile_size)
+}
+
+fn (app App) tile_y_to_px (y int) int {
+    return y * app.map.tile_size
+}
+
+fn (app App) tile_x_to_px (x int) int {
+    return x * app.map.tile_size
+}
+
+fn (app App) draw_map () {
+    /*struct Map {
+    mut:
+        tiles [][]Tiles
+        small_plants [][]Small_plants
+        med_plants [][]Med_plants
+        big_plants [][]Big_plants
+        background []Images
+        foreground []Images
+        tile_size int
+        tiles_im []gg.Image
+        small_plants_im []gg.Image
+        med_plants_im []gg.Image
+        big_plants_im []gg.Image
+    }
+    */
+    for y in 0..app.map.tiles.len {
+        for x in 0..app.map.tiles[y].len {
+            app.ctx.draw_image(app.tile_x_to_px(x), app.tile_y_to_px(y), app.map.tile_size, app.map.tile_size, app.map.tiles_im[int(app.map.tiles[y][x])])
+        }
+    }
+}
+
 // fn (ctx &Context) draw_line(x f32, y f32, x2 f32, y2 f32, c Color)
 // fn (ctx &Context) draw_square_filled(x f32, y f32, s f32, c Color)
+// fn (ctx &Context) draw_image(x f32, y f32, width f32, height f32, img_ &Image)
